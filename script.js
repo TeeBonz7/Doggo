d36bd67233b7f6ba2d40f99d2e3e4888882fa041
Zachary Thomas <teebonz7@gmail.com>
2022-05-19 20:33:23 -0700

    here's another commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let image = document.getElementById('pet')
 
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
+// author's note: for some reason, it makes it smaller but the resizing works fine after a few clicks. so it fixes itself out.
 let changeSize = document.getElementById('sizething')
 
 //DO NOT CHANGE THE FOLLOWING CODE
